1a12b6154c9736d93d0f88136455c0e367c8ccf7
Iqbal Fadhil <1445518-iqbal-fadhil@users.noreply.gitlab.com>
2024-10-11 13:05:32 +0700

    Added shortcode for registration

--- includes/registration.php
+++ includes/registration.php
@@ -14,4 +14,39 @@ function custom_user_registration() {
     // Registration logic here
 }
 add_action('init', 'custom_user_registration');
+
+// Shortcode for the registration form
+function membership_registration_form_shortcode() {
+    ob_start();
+
+    if (is_user_logged_in()) {
+        echo '<p>You are already logged in.</p>';
+    } else {
+        // Output registration form
+        ?>
+        <form id="membership-registration-form" action="<?php echo esc_url(admin_url('admin-post.php')); ?>" method="POST">
+            <input type="hidden" name="action" value="membership_register_user">
+            <p>
+                <label for="username">Username</label>
+                <input type="text" name="username" id="username" required>
+            </p>
+            <p>
+                <label for="email">Email</label>
+                <input type="email" name="email" id="email" required>
+            </p>
+            <p>
+                <label for="password">Password</label>
+                <input type="password" name="password" id="password" required>
+            </p>
+            <p>
+                <input type="submit" value="Register">
+            </p>
+        </form>
+        <?php
+    }
+
+    return ob_get_clean();
+}
+add_shortcode('membership_registration_form', 'membership_registration_form_shortcode');
+
 ?>
